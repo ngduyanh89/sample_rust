42f94ed9b902d6f1e07d71d17c0d251e212a16de
Duy Anh <ngduyanh89@gmail.com>
2022-06-12 21:59:49 +0700

    update lession1 and lession2

--- rust-array-subset/src/main.rs
+++ rust-array-subset/src/main.rs
@@ -2,29 +2,22 @@
 fn array_subset(array_a: &[i32], array_b: &[i32]) -> bool {
     let m: i32 = array_a.len().try_into().unwrap();
     let n: i32 = array_b.len().try_into().unwrap();
-
     if m < n {
         return false;
-    }       
-    
-    for i in 0..n - 1 {
-        for j in 0..m-1 {
-            if array_b[i as usize] == array_a[j as usize] {
-                break;
-            }
-        }
-        if i == m {
-            return false;
+    }           
+    for arr in array_a.windows(array_b.len()) {
+        if arr == array_b {
+            return true;
         }
     }
-    return true;
+    return false;
 }
 
 fn main() {
     let array_a: [i32; 8] = [1, 2, 3, 5, 6, 8, 10, 11];
-    let array_b: [i32; 3] = [6,8,11];
+    let array_b: [i32; 3] = [6,8,10];
 
     let result = array_subset(&array_a, &array_b);
-    println!("ket qua  =====>>> {}", &result);
+    println!("Result  =====>>> {}", &result);
 }
 
--- rust-word-count/src/main.rs
+++ rust-word-count/src/main.rs
@@ -5,14 +5,22 @@ use std::io::stdin;
 fn count_words_from_file(file_path: &str, input_word: &str) -> u32 {
     let file = File::open(file_path).expect("error opening the file");
     let reader = BufReader::new(file);
-    let mut word_count: u32 = 0;
+    let mut _word_count: u32 = 0;
     for line in reader.lines() {
         let curr: String = line.expect("error reading content of the file");
-        let words: Vec<&str> = curr.split(' ').collect();
-        let filtered_words: Vec<&str> = words.into_iter().filter(|word| word.len() > 0 && word == &input_word).collect();
-        word_count+= filtered_words.len() as u32
+        let words: Vec<&str> = curr.split(input_word).collect();
+        _word_count+= words.len() as u32 - 1;
     }
-    return word_count
+    return _word_count
+}
+
+
+fn count_words_from_string(input_string: &str, input_word: &str) -> u32 {
+    let mut _word_count: u32 = 0;
+    let words: Vec<&str> = input_string.split(input_word).collect();
+    _word_count = words.len() as u32 - 1;
+    
+    return _word_count;
 }
 
 fn input_line() -> String {
@@ -23,10 +31,16 @@ fn input_line() -> String {
     line
 }
 
-
 fn main() {
-    let file_path = "src/input.txt";
+    // input from terminal
     let input_word = input_line();
-    let word_count = count_words_from_file(file_path, &input_word);
-    println!("{}", word_count);
+    // string to check
+    let input_string = "This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal.";
+    
+    let word_count_from_string = count_words_from_string(input_string, &input_word);
+    println!("Number of word {} in the string is {}", input_word, word_count_from_string);
+
+    let file_path = "src/input.txt";
+    let word_count_from_file = count_words_from_file(file_path, &input_word);
+    println!("Number of word {} in the file's text is {}", input_word, word_count_from_file);
 }
